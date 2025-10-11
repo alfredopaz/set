4d4d458c1326910e3d77eb2976e04a7341218f4e
All23tor <acc.dummyagta1@gmail.com>
2025-10-11 01:55:59 -0500

    Made ArraySet rule of Five compliant and added template parameter for custom equality comparator

--- ArraySet.hpp
+++ ArraySet.hpp
@@ -5,22 +5,33 @@
 #include <cassert>
 #include <vector>
 
-template <class T>
+template <class T, class Eq = std::equal_to<T>>
+requires std::predicate<Eq, const T&, const T&>
 class ArraySet {
   std::vector<T> data;
+  Eq eq;
 
 public:
+  // Thanks std::vector, for making everything so easy!
+  ArraySet() = default;
+  explicit ArraySet(const Eq& _eq) : eq(_eq) {}
+  ArraySet(const ArraySet&) = default;
+  ArraySet(ArraySet&&) noexcept = default;
+  ArraySet& operator=(const ArraySet&) = default;
+  ArraySet& operator=(ArraySet&&) noexcept = default;
+  ~ArraySet() = default;
+
   void insert(const T& value) {
     if (!contains(value))
       data.push_back(value);
   }
 
   void erase(const T& value) {
-    std::erase(data, value);
+    std::erase_if(data, [&](const T& t) { return eq(t, value); });
   }
 
   bool contains(const T& value) const {
-    return std::ranges::contains(data, value);
+    return std::ranges::any_of(data, [&](const T& t) { return eq(t, value); });
   }
 
   bool empty() const {
